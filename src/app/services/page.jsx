2a12e3e12a7b2f9dee08d937e497a17f127fc5fb
khaled sayed <khaledsayed273@gmail.com>
2024-03-18 02:36:27 +0200

    first -v

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -42,14 +42,13 @@ function page() {
         <main className='p-2 md:p-3 mt-10'>
             <div className="container mx-auto text-white md:px-5">
                 <div className="grid md:grid-cols-2 gap-x-2 gap-y-10 md:gap-10 mb-5">
-                    {data.map((item , index) => (
-                        <Link key={item.id} href={`/services/${item.id}`} className={`hover:scale-90 transition-all duration-500 ${index+1 === data.length && data.length % 2 !== 0  ? "md:col-span-2" : "" }`}>
-                        
-                            <div className='relative rounded-2xl overflow-hidden h-[300px] sm:h-[350px] lg:h-[450px]'>
-                            <div className='absolute w-full h-full left-0 top-0 bg-black/40'>
+                    {data?.map((item, index) => (
+                        <Link key={item.id} href={`/services/${item.id}`} className={`hover:scale-90 transition-all duration-500 ${index + 1 === data.length && data.length % 2 !== 0 ? "md:col-span-2" : ""}`}>
 
-                            </div>
-                                <Image  src={item.image} className='w-full h-full top-0' alt={item.title} priority />
+                            <div className='relative rounded-2xl overflow-hidden h-[300px] sm:h-[350px] lg:h-[450px]'>
+                                <div className='absolute w-full h-full left-0 top-0 z-40 bg-black/40'>
+                                </div>
+                                <Image priority sizes='(min-width:992px),100vw' placeholder='blur' fill src={item.image} alt={item.title} />
                             </div>
                             <h1 className='text-center mt-5 text-xl xl:text-4xl font-bold md:font-medium uppercase'>{item.title}</h1>
 
