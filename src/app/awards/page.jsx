3baea2b1d7c60ea65300e0beef8615beca56e327
khaled sayed <khaledsayed273@gmail.com>
2024-03-18 02:53:59 +0200

    first -v

--- src/app/awards/page.jsx
+++ src/app/awards/page.jsx
@@ -40,7 +40,7 @@ function page() {
         <main className='p-2 md:p-3'>
             <div className="container mx-auto text-white md:px-5">
                 <div className="relative w-full h-[450px] sm:h-[400px] lg:h-[500px] xl:h-[700px] my-8 xl:my-2 overflow-hidden rounded-xl border border-amber-500">
-                    <Image priority style={{ objectFit: "cover" }} sizes='(max-width:992px), 100vw' fill src={awardImage1} alt='image-award' />
+                    <Image placeholder='blur' priority style={{ objectFit: "cover" }} sizes='(max-width:992px), 100vw' fill src={awardImage1} alt='image-award' />
                     <div className='absolute flex  left-0 top-0 right-0 bottom-0 bg-black/35 '>
 
                     </div>
@@ -53,7 +53,7 @@ function page() {
                     {data.map((item) => (
                         <div key={item.id} >
                             <div className="relative h-[400px] xl:h-[600px] w-full overflow-hidden rounded-2xl">
-                                <Image priority sizes='(max-width:992px), 100vw' fill src={item.image} alt={item.title} />
+                                <Image placeholder='blur' priority sizes='(max-width:992px), 100vw' fill src={item.image} alt={item.title} />
                             </div>
                             <h1 className='text-center mt-3 capitalize text-2xl'>{item.title}</h1>
                             <p className='text-center mt-3 text-sm md:text-base px-2 xl:px-10'>{item.about} </p>
