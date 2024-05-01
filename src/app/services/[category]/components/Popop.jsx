e6102f73cc506981b237732809b176c2afc13c68
khaled sayed <khaledsayed273@gmail.com>
2024-05-01 06:57:36 +0300

    version 1.0

--- src/app/services/[category]/components/Popop.jsx
+++ src/app/services/[category]/components/Popop.jsx
@@ -7,14 +7,14 @@ import Image from 'next/image'
 function Popop({img, isOpen, closeModal }) {
 
   return (
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+    <Transition  appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={closeModal}>
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-in duration-300"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-300"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
@@ -25,16 +25,16 @@ function Popop({img, isOpen, closeModal }) {
           <div className="flex h-screen items-center justify-center p-4 text-center">
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
+              enter="ease-in duration-300"
+              enterFrom="opacity-0 scale-50"
               enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
+              leave="ease-in duration-300"
               leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
+              leaveTo="opacity-0 scale-50"
             >
-              <Dialog.Panel className="w-full h-[70vh] relative max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 shadow-xl transition-all">
+              <Dialog.Panel className="w-full h-[70vh]  relative max-w-xl transform overflow-hidden rounded-2xl  p-6 shadow-xl transition-all">
               
-                <Image fill src={img} alt='img'/>
+                <Image  fill src={img} alt='img'/>
 
                 
               </Dialog.Panel>
