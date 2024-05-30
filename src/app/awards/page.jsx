14c2c94e07207262e1ef5249f45c53e631312988
khaled sayed <khaledsayed273@gmail.com>
2024-05-30 19:40:43 +0300

    first -v

--- src/app/awards/page.jsx
+++ src/app/awards/page.jsx
@@ -64,8 +64,8 @@ async function page() {
                         <div className="grid md:grid-cols-2 gap-5 gap-y-10 my-16">
                             {awards.data.map((item) => (
                                 <div data-aos="fade-up" data-aos-duration="1000" key={item.id} >
-                                    <div className="relative h-[400px] xl:h-[600px] w-full overflow-hidden rounded-2xl">
-                                        <Image priority sizes='(max-width:992px), 100vw' fill src={item.image} alt={item.name} />
+                                    <div className="relative h-[600px] xl:h-[900px] w-full overflow-hidden rounded-2xl">
+                                        <Image style={{objectFit: "fill"}} priority sizes='(max-width:992px), 100vw' fill src={item.image} alt={item.name} />
                                     </div>
                                     <h1 className='text-center mt-3 capitalize text-2xl'>{item.name}</h1>
                                     <p className='text-center mt-3 text-sm md:text-base px-2 xl:px-10'>{item.description} </p>
