3bcedbb24c76e76cb210dc18b5f75627156dba40
khaled sayed <khaledsayed273@gmail.com>
2024-06-02 17:45:37 +0300

    first -v

--- src/app/awards/page.jsx
+++ src/app/awards/page.jsx
@@ -30,7 +30,7 @@ async function page() {
             <div className="container px-2 mx-auto text-white md:px-5">
                 {awards?.status ? (
                     <>
-                        <div className="grid md:grid-cols-2 gap-5 gap-y-10 my-16">
+                        <div className="grid md:grid-cols-2 gap-5 md:gap-10 gap-y-10 my-16">
                             {awards.data.map((item) => (
                                 <div data-aos="fade-up" data-aos-duration="1000" key={item.id} >
                                     <div className="relative h-[600px] xl:h-[900px] w-full overflow-hidden rounded-2xl">
